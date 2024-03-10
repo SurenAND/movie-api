fdd54460d644ec917a78595b4bb5cd3f61b7a9ec
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-10 17:11:23 +0330

    create movie card in renderMovies function

--- src/main.js
+++ src/main.js
@@ -9,4 +9,31 @@ function renderMovies(rawData) {
   // clear movies section
   const movieSection = document.getElementById("movies");
   movieSection.innerHTML = "";
+
+  if (data.length === 0) {
+    const notFound = document.createElement("h2");
+    notFound.classList.add("text-6xl", "font-black", "text-blue-500");
+    notFound.innerText = "Movie Does Not Found!😕";
+    movieSection.append(notFound);
+  } else {
+    data.forEach((movie) => {
+      // create movie card
+      const movieCard = document.createElement("div");
+      movieCard.classList.add(
+        "flex",
+        "flex-col",
+        "items-center",
+        "text-center",
+        "sm:text-left",
+        "sm:items-start",
+        "sm:flex-row",
+        "w-full",
+        "rounded-lg",
+        "text-left",
+        "bg-gray-700",
+        "text-white",
+        "p-4"
+      );
+    });
+  }
 }
